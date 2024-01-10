6cb8b2d375110e8d0f25473606ae2e949eee2f79
Rafael Pereira Satyro <rafael.satyro@hotmail.com>
2024-01-10 01:27:06 -0300

    first-commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+import SpAcSheet from './src/components/SplashAndActionSheet'
+
+export default function App(){
+    return(
+        <View>
+            <SpAcSheet/>
+        </View>
+    );
+}
