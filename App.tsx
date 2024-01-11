bdda3be9cb388cd6831e07d2e314963945392271
Rafael Pereira Satyro <rafael.satyro@hotmail.com>
2024-01-11 19:56:51 -0300

    teste/feature/navegação

--- App.tsx
+++ App.tsx
@@ -1,11 +1,22 @@
 import React from "react";
-import { View, Text } from "react-native";
-import SpAcSheet from './src/components/SplashAndActionSheet'
+import Login from "./src/components/Login";
+import Dashboard from "./src/components/Dashboard";
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+const Stack = createNativeStackNavigator();
+    
 export default function App(){
+    
     return(
-        <View>
-            <SpAcSheet/>
-        </View>
+        <>
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Login" screenOptions={{headerShown: false}}>
+                <Stack.Screen name="Login" component={Login} />
+                <Stack.Screen name="Dashboard" component={Dashboard} />
+            </Stack.Navigator>
+        </NavigationContainer>
+        </>
     );
 }
